41e0853b973bb7d7b7a3645852380835afe8ce65
monika-stempniak <mstempniak@gmail.com>
2019-06-03 15:13:00 +0200

    Install import redux-devtools-extension to fix store bug with devtools enhancers

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,12 @@
-import { applyMiddleware, compose, createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./reducers";
 
 const middlewares = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 export const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    composeEnhancers(applyMiddleware(...middlewares))
-  )
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
